071cd2bce07de4a1459c604451d3503c8f9b96aa
juan julio <juanmontreuil71@gmail.com>
2020-12-22 12:06:04 -0600

    Enviamos nuestros cambios al repositorio local gradle

---
+++ src/main/java/HolaMundoGradle/Main.java
@@ -0,0 +1,15 @@
+
+package HolaMundoGradle;
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Juan esto es Gradle");
+    }
+    
+}
